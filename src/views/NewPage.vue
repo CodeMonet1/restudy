a7d3b23ab5f82f5a2660da964dcef68e8837c025
hch <1975410865@qq.com>
2024-01-03 14:25:04 +0800

    yz分支提交

--- src/views/NewPage.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="new">
-    <h1>This is an New page</h1>
-  </div>
-</template>
---
+++ src/views/YZNewPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="yz">
+      <h1>This is an yz page</h1>
+    </div>
+  </template>
