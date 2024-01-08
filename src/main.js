6f261ac0858c3ea2dd7d96bb58b6742392d3dc50
hch <1975410865@qq.com>
2024-01-08 14:29:01 +0800

    1.8

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 import {dayjs,yz_test} from  "@/assets/util/yz_main.js"
-
+// Vue.component("Treeselect", Treeselect);
 const now = dayjs
 console.log(dayjs('1995-12-25'));
 console.log(yz_test.name);
--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
+  <div id="about">
     <h1 class="bg_pink font_green">This is an movie page</h1>
+
+    <treeselect v-model="value" :multiple="true" :options="options" />
   </div>
 </template>
+
+<script>
+  // import the component
+  import Treeselect from '@/assets/util/@riophae/vue-treeselect'
+  // import the styles
+  import '@/assets/util/@riophae/vue-treeselect/dist/vue-treeselect.css'
+
+  export default {
+    // register the component
+    components: { Treeselect },
+    data() {
+      return {
+        // define the default value
+        value: null,
+        // define options
+        options: [ {
+          id: 'a',
+          label: 'a',
+          children: [ {
+            id: 'aa',
+            label: 'aa',
+          }, {
+            id: 'ab',
+            label: 'ab',
+          } ],
+        }, {
+          id: 'b',
+          label: 'b',
+        }, {
+          id: 'c',
+          label: 'c',
+        } ],
+      }
+    },
+  }
+</script>
